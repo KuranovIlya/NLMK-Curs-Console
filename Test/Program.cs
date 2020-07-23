924253465c32de0884310609778e6ce818ef5db7
KuranovIlya <43750944+KuranovIlya@users.noreply.github.com>
2020-07-23 17:27:02 +0300

    Add files via upload

---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static DataBase db;
+        static void Main(string[] args)
+        {
+            db = new DataBase();
+            db.CreateBase();
+            db.AddCurs();
+
+            Console.WriteLine("Синхронизация завершена! Нажмите любую кнопку, чтобы продолжить.");
+            Console.Read();
+        }
+
+        
+       
+    }
+}
